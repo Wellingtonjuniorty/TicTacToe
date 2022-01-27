90af649bbac35bf47d4e0f622e5de630ca9ee8bf
Wellington Júnior <83925986+Wellingtonjuniorty@users.noreply.github.com>
2022-01-27 20:55:39 -0300

    Correção de Erro
    Quando todos os quadrados eram ocupados, a ultima rodada sempre dava velha.

--- tic-tac-toe/src/TicTacToe.js
+++ tic-tac-toe/src/TicTacToe.js
@@ -3,9 +3,10 @@ import './TicTacToe.css';
 
 export default function TicTacToe() {
 	var player1 = 'X'
-	const [board, setBoard] = useState(Array(9).fill(""));
+	const [board, setBoard] = useState(Array(9).fill(''));
 	const [currentPlayer, setCurrentPlayer] = useState(player1);
-	const [winner, setWinner] = useState()
+	const [winner, setWinner] = useState(null)
+
 	function CellClick(index) {
 		if (winner) return null;
 		if (board[index] !== '') return null;
@@ -14,27 +15,30 @@ export default function TicTacToe() {
 	}
 	function checkResult() {
 		const possibleWaysToWin = [
-			[board[0], board[1], board[2]],
-			[board[3], board[4], board[5]],
-			[board[6], board[7], board[8]],
-
-			[board[0], board[3], board[6]],
-			[board[1], board[4], board[7]],
-			[board[2], board[5], board[8]],
-
-			[board[0], board[4], board[8]],
-			[board[6], board[4], board[2]]
+			board[0]+ board[1]+ board[2],
+			board[3]+ board[4]+ board[5],
+			board[6]+ board[7]+ board[8],
+					
+			board[0]+ board[3]+ board[6],
+			board[1]+ board[4]+ board[7],
+			board[2]+ board[5]+ board[8],
+					
+			board[0]+ board[4]+ board[8],
+			board[6]+ board[4]+ board[2]
 		];
-		possibleWaysToWin.forEach(cells => {
-			if (cells.every(cell => cell === 'O')) setWinner('O venceu !');
-			if (cells.every(cell => cell === 'X')) setWinner('X venceu !');
-		})
-		if (!board.includes("") && winner === undefined) setWinner('Deu Velha !');
+		if (possibleWaysToWin.includes('XXX')) {
+			setWinner('X venceu !')
+		}
+		else if (possibleWaysToWin.includes('OOO')) {
+			setWinner('O venceu !')
+		} else if (winner === null && !board.includes("")) {
+			setWinner('Deu velha !')
+		}
 	}
 	function resetGame() {
 		setCurrentPlayer(player1)
 		setBoard(Array(9).fill(""));
-		setWinner(undefined);
+		setWinner(null);
 	}
 	useEffect(checkResult, [board]);
 	return (
@@ -50,7 +54,7 @@ export default function TicTacToe() {
 					</div>))}
 			</div>
 			<div className="footer">
-				<button on onClick={resetGame}>Recomeçar</button>
+				<button onClick={resetGame}>Recomeçar</button>
 				{
 					winner &&
 					<h2 className="winner-message">
